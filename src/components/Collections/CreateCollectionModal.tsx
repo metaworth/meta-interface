6b8b7b5d8fa1379ffced64ee556ed5e88c587759
Samdanae Imran <samdanae@gmail.com>
2021-11-15 16:51:45 +1300

    Add create collection modal and custom theme

---
+++ src/components/Collections/CreateCollectionModal.tsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  ModalHeader,
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+
+interface CreateCollectionModalProps {
+  isOpen: boolean;
+  onClose: VoidFunction;
+}
+
+const CreateCollectionModal: React.FC<CreateCollectionModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  const [collectionName, setCollectionName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const createCollection = (event: any) => {
+    event.preventDefault();
+    alert(
+      `Form submitted with collection name: ${collectionName} and description: ${description}`
+    );
+    onClose();
+  };
+
+  const formId = 'create-collection-form';
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size={'full'}>
+      <ModalOverlay />
+      <ModalContent alignItems='center'>
+        <Box maxW='sm' width='sm' marginTop='36'>
+          <ModalHeader marginBottom='16' padding='0' fontSize='2xl'>
+            <Center>Create your collection </Center>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form onSubmit={createCollection} id={formId}>
+              <FormControl isRequired>
+                <FormLabel fontSize='sm'>Collection name</FormLabel>
+                <Input
+                  placeholder='Name your collection'
+                  onChange={(event) =>
+                    setCollectionName(event.currentTarget.value)
+                  }
+                  fontSize='sm'
+                  marginBottom='5'
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel fontSize='sm'>Description</FormLabel>
+                <Textarea
+                  fontSize='sm'
+                  onChange={(event) =>
+                    setDescription(event.currentTarget.value)
+                  }
+                />
+              </FormControl>
+            </form>
+          </ModalBody>
+          <ModalFooter justifyContent='space-between'>
+            <Button colorScheme='meta' variant='outline' onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme='meta' type='submit' form={formId}>
+              Submit
+            </Button>
+          </ModalFooter>
+        </Box>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CreateCollectionModal;
--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -1,10 +1,25 @@
-import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+import { Colors, extendTheme, ThemeConfig } from '@chakra-ui/react';
 
-const config : ThemeConfig = {
+const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: false,
-}
+};
 
-const theme = extendTheme({ config })
+const colors: Colors = {
+  meta: {
+    50: '#4ad3a616',
+    100: '#f7fafc',
+    // 200: 'blue',
+    // 300: 'red',
+    // 400: 'green',
+    500: '#4AD3A6',
+    600: '#3BD3A5',
+    700: '#dddfe2',
+    // 800: 'black',
+    900: '#1a202c',
+  },
+};
 
-export default theme
+const theme = extendTheme({ config, colors });
+
+export default theme;
